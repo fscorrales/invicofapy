29b92f44c9ca6d7046301d1e3438da26acdd34e3
Fernando Corrales <fscorrales@gmail.com>
2025-02-13 20:26:27 -0300

    add go to reports in connect siif module

--- src/siif/utils/connect.py
+++ src/siif/utils/connect.py
@@ -10,7 +10,6 @@ Purpose: Connect to SIIF
 
 import argparse
 import asyncio
-import time
 from dataclasses import dataclass
 
 from playwright._impl._browser import Browser, BrowserContext, Page
@@ -38,7 +37,7 @@ def get_args():
 class ConnectSIIF:
     browser: Browser = None
     context: BrowserContext = None
-    page: Page = None
+    home_page: Page = None
 
 
 # --------------------------------------------------
@@ -54,20 +53,45 @@ async def login(
     context = await browser.new_context(no_viewport=True)
     page = await context.new_page()
 
-    "Open SIIF webpage"
-    await page.goto("https://siif.cgpc.gob.ar/mainSiif/faces/login.jspx")
-
     try:
+        "Open SIIF webpage"
+        await page.goto("https://siif.cgpc.gob.ar/mainSiif/faces/login.jspx")
+        "Login with credentials"
         await page.locator("id=pt1:it1::content").fill(username)
         await page.locator("id=pt1:it2::content").fill(password)
         btn_connect = page.locator("id=pt1:cb1")
         await btn_connect.click()
+        await page.wait_for_load_state("networkidle")
     except Exception as e:
         print(f"Ocurrio un error: {e}")
         # await context.close()
         # await browser.close()
 
-    return ConnectSIIF(browser=browser, context=context, page=page)
+    return ConnectSIIF(browser=browser, context=context, home_page=page)
+
+
+# --------------------------------------------------
+async def go_to_reports(connect: ConnectSIIF) -> None:
+    try:
+        btn_reports = connect.home_page.locator("id=pt1:cb12")
+        await btn_reports.wait_for()
+        await btn_reports.click()
+        await connect.home_page.wait_for_load_state("networkidle")
+        # New Tab generated
+        async with connect.context.expect_page() as new_page_info:
+            btn_ver_reportes = connect.home_page.locator("id=pt1:cb14")
+            await btn_ver_reportes.wait_for()
+            await btn_ver_reportes.click()  # Opens a new tab
+        reports_page = await new_page_info.value
+        all_pages = connect.context.pages
+        print(f"La lista de paginas es: {all_pages}")
+    except Exception as e:
+        print(f"Ocurrio un error: {e}")
+
+
+# --------------------------------------------------
+async def logout(connect: ConnectSIIF) -> None:
+    await connect.home_page.locator("id=pt1:pt_np1:pt_cni1").click()
 
 
 # --------------------------------------------------
@@ -80,11 +104,15 @@ async def main():
     print(args.password)
 
     async with async_playwright() as p:
-        await login(args.username, args.password, playwright=p, headless=False)
-        # sleep time 10 seconds
-        time.sleep(10)
+        connect_siif = await login(
+            args.username, args.password, playwright=p, headless=False
+        )
+        await go_to_reports(connect=connect_siif)
+        await logout(connect=connect_siif)
 
 
 # --------------------------------------------------
 if __name__ == "__main__":
     asyncio.run(main())
+    # From /invicofapy
+    # .venv/Scripts/python src/siif/utils/connect.py username password
