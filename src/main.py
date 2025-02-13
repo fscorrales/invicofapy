a4dfe032c15210399f750287903ada5dbf542f0f
Fernando Corrales <fscorrales@gmail.com>
2025-02-13 10:31:55 -0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+# from .api.routes import api_router, auth_router
+
+# tags_metadata = [
+#     {"name": "Auth"},
+#     {"name": "Users"},
+#     {"name": "Products"},
+# ]
+
+# app = FastAPI(title="Final Project API", openapi_tags=tags_metadata)
+app = FastAPI(title="Final Project API")
+
+# # Include our API routes
+# app.include_router(api_router)
+# # Let's include our auth routes aside from the API routes
+# app.include_router(auth_router)
+
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", include_in_schema=False)
+def home(request: Request):
+    return {"message": "Hello World"}
