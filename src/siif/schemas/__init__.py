b1039b99b818358481bbfdd3b8b79c2290d19387
Fernando Corrales <fscorrales@gmail.com>
2025-06-19 10:57:14 -0300

    add rfondo07tp's schema

--- src/siif/schemas/__init__.py
+++ src/siif/schemas/__init__.py
@@ -2,3 +2,4 @@ from .common import *
 from .rcg01_uejp import *
 from .rf602 import *
 from .rpa03g import *
+from .rfondo07tp import *
---
+++ src/siif/schemas/rfondo07tp.py
@@ -0,0 +1,72 @@
+__all__ = [
+    "Rfondo07tpReport",
+    "Rfondo07tpDocument",
+    "Rfondo07tpValidationOutput",
+    "Rfondo07tpParams",
+    "Rfondo07tpFilter",
+]
+
+from datetime import date
+from typing import List, Optional
+
+from pydantic import BaseModel, Field, field_validator
+from pydantic_mongo import PydanticObjectId
+
+from ...utils import BaseFilterParams, ErrorsWithDocId
+from .common import TipoComprobanteSIIF
+
+
+# --------------------------------------------------
+class Rfondo07tpParams(BaseModel):
+    ejercicio: int = date.today().year
+    tipo_comprobante: TipoComprobanteSIIF = TipoComprobanteSIIF.adelanto_contratista
+    # ejercicio: int = Field(
+    #     default_factory=lambda: date.today().year,
+    #     alias="ejercicio",
+    #     description="Año del ejercicio fiscal (entre 2010 y el año actual)",
+    #     example=2025,
+    # )
+
+    @field_validator("ejercicio")
+    @classmethod
+    def validate_value(cls, v):
+        current_year = date.today().year
+        if not (2010 <= v <= current_year):
+            raise ValueError(f"Ejercicio debe estar entre 2010 y {current_year}")
+        return v
+
+    def __int__(self):
+        return self.ejercicio
+
+
+# -------------------------------------------------
+class Rfondo07tpReport(BaseModel):
+    ejercicio: int
+    mes: str
+    fecha: date
+    nro_comprobante: str
+    importe: float
+    grupo: str
+    partida: str
+    nro_entrada: Optional[str] = None
+    nro_origen: Optional[str] = None
+    nro_expte: str
+    glosa: str
+    beneficiario: str
+
+
+# -------------------------------------------------
+class Rfondo07tpDocument(Rfondo07tpReport):
+    id: PydanticObjectId = Field(alias="_id")
+
+
+# -------------------------------------------------
+class Rfondo07tpFilter(BaseFilterParams):
+    ejercicio: Optional[int] = None
+    grupo_partida: GrupoPartidaSIIF = None
+
+
+# -------------------------------------------------
+class Rfondo07tpValidationOutput(BaseModel):
+    errors: List[ErrorsWithDocId]
+    validated: List[Rfondo07tpDocument]
