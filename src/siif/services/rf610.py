7c6c3ea7ffffad0d48634a1b1130604c9c2b68d1
Fernando Corrales <fscorrales@gmail.com>
2025-07-01 15:39:02 -0300

    refactor: rename Rf602 to Rf610 in service and repository classes

--- src/siif/services/rf610.py
+++ src/siif/services/rf610.py
@@ -1,4 +1,4 @@
-__all__ = ["Rf602Service", "Rf602ServiceDependency"]
+__all__ = ["Rf610Service", "Rf610ServiceDependency"]
 
 from dataclasses import dataclass, field
 from typing import Annotated, List
@@ -14,27 +14,27 @@ from ...utils import (
     RouteReturnSchema,
     validate_and_extract_data_from_df,
 )
-from ..handlers import Rf602
-from ..repositories import Rf602RepositoryDependency
-from ..schemas import Rf602Document, Rf602Params, Rf602Report
+from ..handlers import Rf610
+from ..repositories import Rf610RepositoryDependency
+from ..schemas import Rf610Document, Rf610Params, Rf610Report
 
 
 # -------------------------------------------------
 @dataclass
-class Rf602Service:
-    repository: Rf602RepositoryDependency
-    rf602: Rf602 = field(init=False)  # No se pasa como argumento
+class Rf610Service:
+    repository: Rf610RepositoryDependency
+    rf610: Rf610 = field(init=False)  # No se pasa como argumento
 
     # -------------------------------------------------
     def __post_init__(self):
-        self.rf602 = Rf602()
+        self.rf610 = Rf610()
 
     # -------------------------------------------------
-    async def sync_rf602_from_siif(
+    async def sync_rf610_from_siif(
         self,
         username: str,
         password: str,
-        params: Rf602Params = None,
+        params: Rf610Params = None,
     ) -> RouteReturnSchema:
         """Downloads a report from SIIF, processes it, validates the data,
         and stores it in MongoDB if valid.
@@ -53,21 +53,21 @@ class Rf602Service:
         return_schema = RouteReturnSchema()
         async with async_playwright() as p:
             try:
-                await self.rf602.login(
+                await self.rf610.login(
                     username=username,
                     password=password,
                     playwright=p,
                     headless=False,
                 )
-                await self.rf602.go_to_reports()
-                await self.rf602.go_to_specific_report()
-                await self.rf602.download_report(ejercicio=str(params.ejercicio))
-                await self.rf602.read_xls_file()
-                df = await self.rf602.process_dataframe()
+                await self.rf610.go_to_reports()
+                await self.rf610.go_to_specific_report()
+                await self.rf610.download_report(ejercicio=str(params.ejercicio))
+                await self.rf610.read_xls_file()
+                df = await self.rf610.process_dataframe()
 
                 # 🔹 Validar datos usando Pydantic
                 validate_and_errors = validate_and_extract_data_from_df(
-                    dataframe=df, model=Rf602Report, field_id="estructura"
+                    dataframe=df, model=Rf610Report, field_id="estructura"
                 )
 
                 # 🔹 Si hay registros validados, eliminar los antiguos e insertar los nuevos
@@ -101,12 +101,12 @@ class Rf602Service:
                     detail="Invalid credentials or unable to authenticate",
                 )
             finally:
-                if hasattr(self.rf602, "logout"):
-                    await self.rf602.logout()
+                if hasattr(self.rf610, "logout"):
+                    await self.rf610.logout()
                 return return_schema
 
     # -------------------------------------------------
-    async def get_rf602_from_db(self, params: BaseFilterParams) -> List[Rf602Document]:
+    async def get_rf610_from_db(self, params: BaseFilterParams) -> List[Rf610Document]:
         try:
             return await self.instruments.find_with_filter_params(params=params)
         except Exception as e:
@@ -117,65 +117,4 @@ class Rf602Service:
             )
 
 
-Rf602ServiceDependency = Annotated[Rf602Service, Depends()]
-
-
-# class Rf602Service:
-#     def __init__(self) -> None:
-#         assert (collection_name := "siif_rf602") in settings.COLLECTIONS
-#         self.collection = db[collection_name]
-#         self.rf602 = Rf602()
-
-#     async def download_and_update(
-#         self, ejercicio: int = dt.datetime.now().year
-#     ) -> Rf602ValidationOutput:
-#         """Downloads a report from SIIF, processes it, validates the data,
-#         and stores it in MongoDB if valid.
-
-#         Args:
-#             ejercicio (int, optional): The fiscal year for the report. Defaults to the current year.
-
-#         Returns:
-#             ValidationResultRf602: A structured response containing validated data and errors.
-#         """
-#         async with async_playwright() as p:
-#             try:
-#                 await self.rf602.login(
-#                     username=settings.SIIF_USERNAME,
-#                     password=settings.SIIF_PASSWORD,
-#                     playwright=p,
-#                     headless=False,
-#                 )
-#                 await self.rf602.go_to_reports()
-#                 await self.rf602.go_to_specific_report()
-#                 await self.rf602.download_report(ejercicio=str(ejercicio))
-#                 await self.rf602.read_xls_file()
-#                 df = await self.rf602.process_dataframe()
-
-#                 # 🔹 Validar datos usando Pydantic
-#                 validate_and_errors = validate_and_extract_data_from_df(
-#                     dataframe=df, model=StoredRf602, field_id="estructura"
-#                 )
-
-#                 # 🔹 Si hay registros validados, eliminar los antiguos e insertar los nuevos
-#                 if validate_and_errors.validated:
-#                     await self.collection.delete_many({"ejercicio": ejercicio})
-#                     validated_records = jsonable_encoder(validate_and_errors.validated)
-#                     inserted_records = await self.collection.insert_many(
-#                         validated_records
-#                     )
-#                     logger.info(
-#                         f"Inserted {len(inserted_records.inserted_ids)} records into MongoDB."
-#                     )
-
-#             except ValidationError as e:
-#                 logger.error(f"Validation Error: {e}")
-#             except Exception as e:
-#                 logger.error(f"Error during report processing: {e}")
-#             finally:
-#                 if hasattr(self.rf602, "logout"):
-#                     await self.rf602.logout()
-#         return validate_and_errors
-
-
-# Rf602ServiceDependency = Annotated[Rf602Service, Depends()]
+Rf610ServiceDependency = Annotated[Rf610Service, Depends()]
