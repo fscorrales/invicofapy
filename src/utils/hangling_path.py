8ef9830415e87e93c0a76033a90f642a654ac473
Fernando Corrales <fscorrales@gmail.com>
2025-07-13 20:28:55 -0300

    rename utils's get_db_path to get_sqlite_path

--- src/utils/hangling_path.py
+++ src/utils/hangling_path.py
@@ -1,16 +1,23 @@
-__all__ = ["get_utils_path", "get_src_path", "get_outside_path", "get_download_path", "get_download_sgf_path","get_db_path", "get_r_icaro_path", "get_slave_path"]
+__all__ = [
+    "get_utils_path",
+    "get_src_path",
+    "get_outside_path",
+    "get_download_path",
+    "get_download_sgf_path",
+    "get_sqlite_path",
+    "get_r_icaro_path",
+    "get_slave_path",
+]
 
-import os
 import inspect
+import os
 
 
 # --------------------------------------------------
 def get_utils_path():
-    dir_path = os.path.dirname(
-        os.path.abspath(
-            inspect.getfile(
-                inspect.currentframe())))
+    dir_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     return dir_path
+
 
 # --------------------------------------------------
 def get_src_path():
@@ -18,41 +25,45 @@ def get_src_path():
     dir_path = os.path.dirname(dir_path)
     return dir_path
 
+
 # --------------------------------------------------
 def get_outside_path():
     dir_path = os.path.dirname(os.path.dirname(get_src_path()))
     return dir_path
 
+
 # --------------------------------------------------
 def get_download_path():
-    dir_path = os.path.join(
-        get_outside_path(),'Reportes Descargados'
-    )
+    dir_path = os.path.join(get_outside_path(), "Reportes Descargados")
     return dir_path
 
 
 # --------------------------------------------------
 def get_download_sgf_path():
-    dir_path = os.path.join(
-        get_download_path(),'Sistema Gestion Financiera'
-    )
+    dir_path = os.path.join(get_download_path(), "Sistema Gestion Financiera")
     return dir_path
 
+
 # --------------------------------------------------
-def get_db_path():
-    db_path = os.path.join(get_outside_path() ,'Python Output')
-    db_path = os.path.join(db_path, 'SQLite Files')
+def get_sqlite_path():
+    db_path = os.path.join(get_outside_path(), "Python Output")
+    db_path = os.path.join(db_path, "SQLite Files")
     return db_path
+
 
 # --------------------------------------------------
 def get_r_icaro_path():
-    dir_path = r'\\192.168.0.149\Compartida CONTABLE\R Apps (Compartida)\R Output\SQLite Files'
+    dir_path = (
+        r"\\192.168.0.149\Compartida CONTABLE\R Apps (Compartida)\R Output\SQLite Files"
+    )
     return dir_path
+
 
 # --------------------------------------------------
 def get_slave_path():
-    dir_path = r'\\192.168.0.149\Compartida CONTABLE\Slave'
+    dir_path = r"\\192.168.0.149\Compartida CONTABLE\Slave"
     return dir_path
+
 
 # --------------------------------------------------
 def main():
@@ -63,9 +74,10 @@ def main():
     print(f"Outside Path: {get_outside_path()}")
     print(f"Download Path: {get_download_path()}")
     print(f"Download SGF Path: {get_download_sgf_path()}")
-    print(f"DB Path: {get_db_path()}")
+    print(f"DB Path: {get_sqlite_path()}")
     print(f"R Icaro Path: {get_r_icaro_path()}")
     print(f"Slave Path: {get_slave_path()}")
+
 
 # --------------------------------------------------
 if __name__ == "__main__":
