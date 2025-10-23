61d0d01a3f6209e0ddc325c1b0a16c77f1d4890b
Fernando Corrales <fscorrales@gmail.com>
2025-10-23 10:40:55 -0300

    feat: Restore compute and export endpoints in ControlHonorarios router

--- src/analisis/routes/control_honorarios.py
+++ src/analisis/routes/control_honorarios.py
@@ -34,25 +34,25 @@ async def sync_control_honorarios_from_source(
     return await service.sync_control_honorarios_from_source(params=params)
 
 
-# # -------------------------------------------------
-# @control_honorarios_router.post("/compute", response_model=List[RouteReturnSchema])
-# async def compute_all(
-#     service: ControlHaberesServiceDependency,
-#     params: Annotated[ControlHaberesParams, Depends()],
-# ):
-#     return await service.compute_all(params=params)
+# -------------------------------------------------
+@control_honorarios_router.post("/compute", response_model=List[RouteReturnSchema])
+async def compute_all(
+    service: ControlHonorariosServiceDependency,
+    params: Annotated[ControlHonorariosParams, Depends()],
+):
+    return await service.compute_all(params=params)
 
 
-# # -------------------------------------------------
-# @control_honorarios_router.get(
-#     "/export",
-#     summary="Descarga todos los controles como archivo .xlsx y exporta a Google Sheets",
-#     response_description="Archivo Excel con los registros solicitados",
-# )
-# async def export_all_from_db(
-#     service: ControlHaberesServiceDependency,
-#     upload_to_google_sheets: bool = Query(True, alias="uploadToGoogleSheets"),
-# ):
-#     return await service.export_all_from_db(
-#         upload_to_google_sheets=upload_to_google_sheets
-#     )
+# -------------------------------------------------
+@control_honorarios_router.get(
+    "/export",
+    summary="Descarga todos los controles como archivo .xlsx y exporta a Google Sheets",
+    response_description="Archivo Excel con los registros solicitados",
+)
+async def export_all_from_db(
+    service: ControlHonorariosServiceDependency,
+    upload_to_google_sheets: bool = Query(True, alias="uploadToGoogleSheets"),
+):
+    return await service.export_all_from_db(
+        upload_to_google_sheets=upload_to_google_sheets
+    )
